b728e9ccbe93d850382f5bc3c4a0e3801624f015
Hector SANCHEZ <hectorsanchez@hectors-mbp.lan>
2020-08-15 01:54:45 -0400

    Adds create user endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :short_urls, only: [:index, :show, :create, :destroy]
+      resources :users, only: :create
     end
   end
 end
---
+++ spec/requests/api/v1/users_request_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Users", type: :request do
+  describe "POST /users" do
+    context "when the user data is valid" do
+      it "creates the user" do
+        post api_v1_users_path, 
+              params: {
+                user: {
+                  email: FFaker::Internet.email,
+                  password: FFaker::Internet.password
+                }
+              }, as: :json
+        
+        expect(response).to have_http_status :created
+      end
+    end
+
+    context "when the user data is invalid" do
+      before do
+        @user = create(:user)
+      end
+
+      it "fails to create the user" do
+        post api_v1_users_path, 
+              params: {
+                user: {
+                  email: @user.email,
+                  password: FFaker::Internet.password
+                }
+              }, as: :json
+        
+        expect(response).to have_http_status :unprocessable_entity
+
+        json_response_error = JSON.parse(response.body)["errors"]
+
+        expect(json_response_error["email"]).to eq ["has already been taken"]
+      end
+    end
+  end
+end
